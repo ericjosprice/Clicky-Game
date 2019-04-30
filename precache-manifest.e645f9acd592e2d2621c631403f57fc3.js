e42b615ab68d2085ec511dca91fbd66acff0a738
ericjosprice <epriceutexas@gmail.com>
2019-04-30 07:10:56 -0500

    Updates

---
+++ precache-manifest.e645f9acd592e2d2621c631403f57fc3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b30425831688d5c4ece71473394a7d8",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "26f84eaa0883cc520dc3",
+    "url": "/Clicky-Game/static/css/main.69aebe2e.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/Clicky-Game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "26f84eaa0883cc520dc3",
+    "url": "/Clicky-Game/static/js/main.3f859783.chunk.js"
+  },
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  }
+]);
